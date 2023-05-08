09ac27e5069d420e05df28561b7e4dc89f6f2eb4
velomovies <22169955+velomovies@users.noreply.github.com>
2023-05-08 16:37:26 +0200

    feat(ConfigScreen.tsx): add config for placement

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,15 +6,46 @@ export enum PageType {
   SettingsAreaSidebarItemGroups = 'settingsAreaSidebarItemGroups',
 }
 
+export enum PlacementType {
+  Before = 'before',
+  After = 'after',
+  MenuItems = 'menuItems',
+  Settings = 'settings',
+  Environment = 'environment',
+  Project = 'project',
+  Permissions = 'permissions',
+  Webhooks = 'webhooks',
+  Deployment = 'deployment',
+  SSO = 'sso',
+  AuditLog = 'auditLog',
+  Usage = 'usage',
+  Content = 'content',
+  MediaArea = 'mediaArea',
+  ApiExplorer = 'apiExplorer',
+}
+
 export type GlobalParameters = {
   pageType?: PageTypeOption
+  pageGroupName?: string
   pageName?: string
   pageSlug?: string
   iconName?: AwesomeFont5SolidIconIdentifier
+  placement?: PlacementOption
+  menuItemPlacement?: MenuItemPlacementOption
   pageEmbedUrl?: string
 }
 
 export type PageTypeOption = {
   value: PageType
+  label: string
+}
+
+export type PlacementOption = {
+  value: PlacementType.Before | PlacementType.After
+  label: string
+}
+
+export type MenuItemPlacementOption = {
+  value: Omit<PlacementType, PlacementType.Before | PlacementType.After>
   label: string
 }
